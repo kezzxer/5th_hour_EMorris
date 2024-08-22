3d134c2e61fd3e2b78ff0d631d152b23a668b391
ethan morris <ethanx420@yahoo.com>
2024-08-22 12:58:42 -0500

    added hw2

---
+++ Hw2 template.py
@@ -0,0 +1,14 @@
+#name: Ethan Morris
+#class: 5th hour
+#assignment: HW2
+
+print("hello world")
+print("what is your name?")
+input()
+
+x = int(input("pick a number"))
+y = int(input("pick another number"))
+
+sum = x + y
+
+print("the outcome is", sum)
