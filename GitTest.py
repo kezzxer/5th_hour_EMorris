73711f7a75b65f977232ccba153a44f0c2dbbebb
ethan morris <ethanx420@yahoo.com>
2024-08-22 13:12:17 -0500

    added GitTest

---
+++ GitTest.py
@@ -0,0 +1,3 @@
+#This is a git test to fix your git commit issues
+
+print("Test from Coach Mack")
---
+++ HW3.py
@@ -0,0 +1,15 @@
+
+
+
+print("hello world")
+naughtyList = ['preston','gabe','ben','ethan','vyse']
+print(naughtyList)
+naughtyList.append(input())
+print(f"{naughtyList[4]}")
+numList = [1,2,3,4,]
+numList.insert(0, 5)
+print(numList)
+numList.sort()
+print(numList)
+sum = (numList[0]) + (numList[1]) + (numList[2])
+print(sum)
