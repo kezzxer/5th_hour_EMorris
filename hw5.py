8a1480f95cf2f24fb7e9d1d761da6ff6c4e5a905
ethan morris <Ethanx420@yahoo.com>
2024-09-27 13:03:05 -0500

    added hm5

---
+++ hw5.py
@@ -0,0 +1,12 @@
+#Name:ethan morris
+#class: 5th hour
+#Assignment: hw5
+
+print("hello world")
+myList = [420,421,419]
+if myList[0] > myList[1] and myList[0] > myList[2]:
+    print("the first number in the list in the greatest"),
+elif myList[1] > myList[0] and myList[1] > myList[2]:
+    print("the second number in the list in the greatest"),
+else:
+    print("the third number in the list in the greatest")
