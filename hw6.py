fecf9ecce28b8676cb994301706b6a4e01fb5a7a
ethan morris <Ethanx420@yahoo.com>
2024-10-01 12:48:47 -0500

    added hm6

---
+++ hw6.py
@@ -0,0 +1,30 @@
+#Name:ethan morris
+#class: 5th hour
+#Assignment: hw5
+
+print("hello world")
+
+num = 97
+
+if num %2==0:
+    print(num %3==0)
+    print("divisible by 2 "),
+else:
+    print("not divisible by 3"),
+else:
+    if num % 3 == 0:
+        print("not divisible by 2")
+        print(num % 3 == 0),
+    else:
+        print("not divisible by 2 or 3")
+
+
+
+
+
+
+
+
+
+
+
