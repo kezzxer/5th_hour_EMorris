ba629c2a069ab1cc05249daf1a52f3ad2930b930
ethan morris <Ethanx420@yahoo.com>
2024-10-23 13:02:35 -0500

    added hw8.py

---
+++ hw8.py
@@ -0,0 +1,49 @@
+#Name:ethan morris
+#Class: 5th Hour
+#Assignment: HW8
+from tkinter import Variable
+
+#1. Print Hello World!
+print("Hello World!")
+#2. Take the variables below and change the name of the variables to match the type of
+#variable they are: string, integer, boolean, or list.
+
+#For example, the first one, Var1 = 4, should be:
+#integerVar1 = 4 or intVar1 = 4
+
+#DO NOT CHANGE THE VALUE OF THE VARIABLE.
+
+intVar1 = 4
+strVar3 = "Box"
+boolVar4 = True
+strVar5 = "Nineteen"
+strVar6 = "Rope"
+intVar7 = 19
+strVar8 = "19"
+listVar9 = ["Window", "Apple", "Penguin"]
+intVar10 = 2
+boolVar11 = True
+boolVar12 = False
+strVar13 = "True"
+strVar14 = "False"
+listVar15 = [1, 0]
+listVar16 = [1]
+listVar17 = [0]
+intVar18 = 5
+strVar19 = "The"
+strVar20 = "Game"
+intVar21 = 3
+intVar22 = 2
+intVar23 = 1
+listVar24 = ["Liftoff", True]
+boolVar25 = boolVar11
+boolVar26 = boolVar12
+strVar27 = strVar13
+strVar28 = strVar14
+listVar29 = listVar15
+intVar30 = 6
+
+#3. Take all the variables you labeled as "integer", add them together, and print the result.
+answer = intVar1 + intVar7 + intVar10 + intVar18 + intVar21 + intVar22 + intVar23 + intVar30
+
+print(answer)
