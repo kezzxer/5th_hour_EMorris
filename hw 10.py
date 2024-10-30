d6fc779003e296251981e35e1eedbde131e97913
ethan morris <Ethanx420@yahoo.com>
2024-10-30 13:18:11 -0500

    added hw10.py

---
+++ hw 10.py
@@ -0,0 +1,27 @@
+
+#Name: Ethan Morris
+#Class: 5th Hour
+#Assignment: HW10
+import time
+#1. Create a while loop with variable i that counts down from 5 to 0
+#and then prints "Hello World!" at the
+#j = 5
+#while j >= 0:
+   # print(j)
+  #  time.sleep(1)
+   # j -= 1
+#else:
+    #print("hello world")
+#2. Create a while loop that prints only even numbers
+#between 1 and 30.
+#j=30
+#while j >= 0:
+  #  print(j)
+   # time.sleep(2)
+    #j -= 2
+#3. Create a while loop that repeats until the user
+#inputs the number 0
+n = int(input("pick a number"))
+while n!=0:
+    print(n)
+    n = int(input("pick another number"))
