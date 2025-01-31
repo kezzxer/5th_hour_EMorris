8a7fdb41348206d1060b20fca8dbd68c087b513a
ethan morris <ethanx420@yahoo.com>
2025-01-31 12:52:25 -0600

    added hw 17

---
+++ hw19.py
@@ -0,0 +1,15 @@
+#Name: ethan morris
+#Class: 5th Hour
+#Assignment: HW19
+
+#1. Import all the functions from the HW15 file.
+from hw15 import hello_world, calculate_average
+from hw15 import animal_list
+from hw15 import loop_to_number
+#2. Call the functions here and run HW19.
+hello_world()
+animal_list("zbra", "wolf", "racoon","monkey","kitty")
+loop_to_number(5)
+
+
+#3. Delete all calls from HW15 and run HW19 agai
